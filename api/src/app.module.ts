1ed779046b30c73f6cbc1c7ae6ac7351339df636
Nicholas Cabral <nicholasscabral@gmail.com>
2025-02-15 15:26:38 -0300

    feat: add api docker boilerplate

---
+++ api/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
